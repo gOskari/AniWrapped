49fdc86c72ca40568d4ece1c419fc17df50e4449
Glad Oskari <s2100959@edu.bc.fi>
2023-09-19 12:13:20 +0300

    Tomii input ja query

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import Data from './Data.js'
+import NameForm from './NameForm.js'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>ok</p>
-      <Data/>
+      <NameForm />
     </main>
   )
 }
