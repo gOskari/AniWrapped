aca22167b8e81bfe4fb7b8de054e8151c059cd7a
gOskari <144109444+gOskari@users.noreply.github.com>
2023-09-25 12:15:41 +0300

    Delete src/app/profile/[slug] directory

--- src/app/profile/[slug]/page.js
+++
@@ -1,7 +0,0 @@
-import Data from '../../Data.js'
-
-export default function Page({ params }) {
-  console.log(params)
-  //return <div>moi {params.slug}</div>
-    return <Data profile_name={params.name} />
-  }
