26c35ce5bf9bd34bd50029444e095c12ad958e5b
Wiltuh <s2101118@edu.bc.fi>
2023-09-28 12:08:53 +0300

    navbar component toimii ehk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
+import Navbar from './NavigationBar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
-import Navbar from './NavigationBar'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navbar/>
-        {children}
-        <footer class="border-t border-gray-300 bg-transparent">
+        <header>
+        <Navbar/>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer class="border-t border-white/20 bg-transparent">
           <div className="container mx-auto text-center">
             <div className="mb-4">
               <a href="https://github.com/gOskari" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-700">
