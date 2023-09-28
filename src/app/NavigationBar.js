28bbf743837dc34695b026678a4841d6e0a51403
Wiltuh <s2101118@edu.bc.fi>
2023-09-28 12:27:10 +0300

    navbar näkyy paremmi

--- src/app/NavigationBar.js
+++ src/app/NavigationBar.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 flex items-center bg-opacity-30 p-4 z-50">
-      <Link href="/" className="text-2xl font-semibold text-white-500 ml-8 hover:text-gray-600">AniWrapped</Link>
+    <nav className="fixed top-0 left-0 flex items-center bg-white/10 w-screen p-4 z-50 backdrop-blur-lg bg-opacity-60">
+      <Link href="/" className="text-2xl font-semibold text-white ml-8 hover:text-gray-400 shadow-text">AniWrapped</Link>
     </nav>
   );
 };
