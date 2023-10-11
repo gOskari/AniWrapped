3e33b07f45e2dbcb1e04300e548a67e440c1cc9c
Wiltuh <s2101118@edu.bc.fi>
2023-10-11 09:32:50 +0300

    Update Data.js

--- src/app/Data.js
+++ src/app/Data.js
@@ -1,38 +1,45 @@
-'use client';
+"use client";
 import Image from "next/image";
 
 //import { Avatar } from 'flowbite-react';
 
-function DataPiece({data}) {
+function DataPiece({ data }) {
   try {
     data = data.data;
     return (
       <div className="w-full h-screen flex items-center justify-center p-10">
-        <div className="grid grid-cols-2 grid-rows-2 gap-10 justify-center items-center w-full h-full rounded-xl">
-          <div className="h-full flex justify-center items-center backdrop-blur-sm bg-white/10 rounded-xl h-1/4">
-            {//<Avatar img={data.avatar} size=""/>
+        <div className="grid grid-cols-2 grid-rows-2 gap-10 justify-center items-center w-full h-full">
+          <div className="h-full flex justify-center items-center backdrop-blur-sm bg-white/10 ">
+            {
+              //<Avatar img={data.avatar} size=""/>
             }
-            <Image src={data.avatar} alt="User Avatar" height="500" width="500" className="object-contain w-full h-full rounded-xl"/>
+            <Image
+              src={data.avatar}
+              alt="User Avatar"
+              height="500"
+              width="500"
+              className="object w-full h-full"
+            />
           </div>
-          <div className="h-full backdrop-blur-sm bg-white/10 row-start-2 flex items-center justify-center rounded-xl">
+          <div className="h-full backdrop-blur-sm bg-white/10 row-start-2 flex items-center justify-center">
             <h1>{data.name}</h1>
           </div>
-          <div className="h-full backdrop-blur-sm bg-white/10 flex items-center justify-center flex-col row-span-2 rounded-xl">
+          <div className="h-full backdrop-blur-sm bg-white/10 flex items-center justify-center flex-col">
             <p>Anime watched: {data.statistics.count}</p>
-            <p>Hours watched: {Math.round((data).statistics.minutesWatched/60, 2)}</p>
+            <p>
+              Hours watched:{" "}
+              {Math.round(data.statistics.minutesWatched / 60, 2)}
+            </p>
           </div>
         </div>
       </div>
     );
-
   } catch (error) {
-    console.error('Datapiece error:', error);
-    return <div>ei toimi</div>
+    console.error("Datapiece error:", error);
+    return <div>ei toimi</div>;
   }
 }
 
-export default function Data({data}) {
-    return (
-      <DataPiece data={data} />
-    );
+export default function Data({ data }) {
+  return <DataPiece data={data} />;
 }
