3f5d8a045c20f3e4eaf41fd5004fd4714b4ec7cf
Wiltuh <s2101118@edu.bc.fi>
2023-10-27 12:41:02 +0300

    Nameform not getto

--- src/app/NameForm.js
+++ src/app/NameForm.js
@@ -14,7 +14,7 @@ export default function NameForm() {
 
   return(
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label for="name" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Name:</label>
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -22,14 +22,9 @@ export default function NameForm() {
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
           </div>
-          <input type="text" id="name" name="name" className="block w-full h-12 p-4 pl-10 text-lg text-secondary-color border border-secondary-color rounded-lg bg-primary-color" placeholder="Anilist Username" />
+          <input type="text" name='route' onChange={(e)=>{setRoute(e.target.value)}} className="block w-full h-12 p-4 pl-10 text-lg text-secondary-color border border-secondary-color rounded-lg bg-primary-color" placeholder="Anilist Username" />
           <button type="submit" className="text-primary-color absolute top-1/2 transform -translate-y-1/2 right-2.5 bg-secondary-color hover:bg-secondary-color-dark focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
         </div>
-      </form>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" name='route' onChange={(e)=>{setRoute(e.target.value)}} />
-        <button type="submit">Submit</button>
       </form>
     </div>
   )
