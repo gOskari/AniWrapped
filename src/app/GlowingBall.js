e7083af08c1809431c2d921bb88e260d655308af
Wiltuh <s2101118@edu.bc.fi>
2023-12-13 12:38:59 +0200

    Better ball

--- src/app/GlowingBall.js
+++ src/app/GlowingBall.js
@@ -4,8 +4,8 @@ const GlowingBallsBackground = () => {
         {/* Background container for glowing balls */}
         <div className="absolute inset-0 overflow-hidden">
           {/* Multiple glowing balls */}
-          <div className="w-96 h-96 rounded-full bg-purple-600 bg-opacity-50 filter blur-xl shadow-neon fixed top-10 left-10"></div>
-          <div className="w-32 h-32 rounded-full bg-blue-600 bg-opacity-50 filter blur-xl shadow-neon fixed top-20 right-20"></div>
+          <div className="w-96 h-96 bg-opacity-50 ball filter blur-3xl fixed top-10 left-20"></div>
+          <div className="w-80 h-80 bg-opacity-50 ball filter blur-3xl fixed bottom-20 right-20"></div>
           {/* Add more balls with different sizes, colors, and positions as needed */}
         </div>
       </div>
